b1425afa31f30bd7f73a58cb73466f8a05c3fef0
Chris Gong <chris_gong@yahoo.com>
2019-08-24 00:29:26 -0400

    Add files via upload

---
+++ photoResistorTest.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+resistorPin = 7
+
+while True:
+    count = 0
+    GPIO.setup(resistorPin, GPIO.OUT)
+    GPIO.output(resistorPin, GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.setup(resistorPin, GPIO.IN)
+    currentTime = time.time()
+    diff = 0
+    while(GPIO.input(resistorPin) == GPIO.LOW):
+        diff = time.time() - currentTime
+    print(diff)
+    time.sleep(1)
+        
