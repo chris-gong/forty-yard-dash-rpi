ce8d903e17e5ddb8efb04b31eca47047b31966b9
Chris Gong <chris_gong@yahoo.com>
2019-09-16 09:39:04 -0400

    Add files via upload

--- photoResistorTest.py
+++ photoResistorTest.py
@@ -6,15 +6,16 @@ GPIO.setmode(GPIO.BOARD)
 resistorPin = 7
 
 while True:
-    count = 0
     GPIO.setup(resistorPin, GPIO.OUT)
     GPIO.output(resistorPin, GPIO.LOW)
     time.sleep(0.1)
+    
     GPIO.setup(resistorPin, GPIO.IN)
     currentTime = time.time()
     diff = 0
+    
     while(GPIO.input(resistorPin) == GPIO.LOW):
-        diff = time.time() - currentTime
-    print(diff)
-    time.sleep(1)
+        diff  = time.time() - currentTime
         
+    print(diff * 1000)
+    time.sleep(1)
